d6404eedf68e95e80b2dd54198c8a670a2cd7396
chenwenjie <chrischen0405@gmail.com>
2018-06-19 16:53:11 +0800

    管理员功能基本实现，除了实验室和计算机的列表联动功能

--- code/WebContent/js/manager.js
+++ code/WebContent/js/manager.js
@@ -4,9 +4,10 @@ var selectcomputer=document.getElementById("computerposition");
 var labname="";
 var computerposition="";
 var studentid="";
-window.onload=withoutend();
-window.onload=loadlab();
-function withoutend(){
+window.onload=loadlab();//页面加载完成后加载第一个选项列表
+window.onload=withoutend();//页面加载完成后显示所有没有完成的记录
+$("#labname").change(changeselect());//当第一个选项改变时改变第二个选项的列表
+function withoutend(){//显示全部没有结束的记录
 	$.ajax({
 		url:"http://localhost:8080/LabManager/loadRecordsWithoutEnd",
 		type:"GET",
@@ -36,9 +37,9 @@ function withoutend(){
 		}
 	});
 }
-function loadlab(){
+function loadlab(){//加载第一个选项列表
 	$.ajax({
-		url:"http://localhost:8080/LabManager/loadAllLab",
+		url:"http://localhost:8080/LabManager/allLab",
 		type:"GET",
 		dataType:"text",
 		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -53,8 +54,8 @@ function loadlab(){
 				for (let i=0; i<arr.length; i++) {
 					var obj=arr[i];
 					var op=document.createElement("option");
-					op.setAttribute("value",obj.labName);
-					op.value=obj.labName;
+					op.value=""+obj.labName;
+					op.innerText=obj.labName;
 					selectlab.appendChild(op);
 				}
 			}
@@ -67,9 +68,104 @@ function loadlab(){
 		}
 	});
 }
-function loadcomputer(){
-	
+function loadcomputer(name){//加载第二个选项列表
+	labname=name;
+	$.ajax({
+		url:"http://localhost:8080/LabManager/getComputersBylabId",
+		data:{
+			labName: labname
+		},
+		type:"GET",
+		dataType:"text",
+		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+		success:function(data, status, xhr){
+			console.log(data);
+			console.log(status);
+			console.log(xhr.status);
+			if(xhr.status == 200) {
+				console.log(xhr.responseText);
+				var arr=eval(xhr.responseText);
+				console.log(arr);
+				for (let i=0; i<arr.length; i++) {
+					var obj=arr[i];
+					var op=document.createElement("option");
+					op.value=""+obj.computerId;
+					op.innerText=obj.computerPosition;
+					selectcomputer.appendChild(op);
+				}
+			}
+			else{
+				console.log(xhr.status);
+			}
+		},
+		error: function (error) {
+			console.log(error);
+		}
+	});	
 }
-function startcomputer(){
-	
+function changeselect(){//第一个列表选项变化时改变第二个列表
+	var index=selectlab.selectedIndex;
+	console.log(index);
+	var value=selectlab.options[index].value;
+	loadcomputer(value);
+}
+function startcomputer(){//开始上机
+	var index1=selectlab.selectedIndex;
+	var value1=selectlab.options[index1].value;
+	var index2=selectcomputer.selectedIndex;
+	var value2=selectcomputer.options[index2].value;
+	var studentid=document.getElementById("studentid");
+	if(checknull(studentid)||!checknumber(studentid)){
+		return false;
+	}
+	$.ajax({
+		url:"http://localhost:8080/LabManager/startRecord",
+		data:{
+			labName: value1,
+			computerPosition: value2,
+			studentId: studentid
+		},
+		type:"GET",
+		dataType:"text",
+		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+		success:function(data, status, xhr){
+			console.log(data);
+			console.log(status);
+			console.log(xhr.status);
+			if(xhr.status == 200) {
+				location.reload();
+			}
+			else{
+				console.log(xhr.status);
+			}
+		},
+		error: function (error) {
+			console.log(error);
+		}
+	});	
+}
+function endcomputer(recordid){//结束上机
+	$.ajax({
+		url:"http://localhost:8080/LabManager/endRecord",
+		data:{
+			recordId: recordid
+		},
+		type:"GET",
+		dataType:"text",
+		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+		success:function(data, status, xhr){
+			console.log(data);
+			console.log(status);
+			console.log(xhr.status);
+			if(xhr.status == 200) {
+				location.reload();
+			}
+			else{
+				console.log(xhr.status);
+			}
+		},
+		error: function (error) {
+			console.log(error);
+		}
+	});
 }
